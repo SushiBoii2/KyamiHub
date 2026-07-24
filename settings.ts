cd3a69e3a533a37e23097aba2f65ff5a9e0ba06e
sushi <glitchycement56@gmail.com>
2026-07-24 19:33:23 +1000

    Update settings.ts

--- settings.ts
+++ settings.ts
@@ -1,3 +1,27 @@
-import { definePluginSettings } from "@api/Settings";
+/*
+ * Vencord Plugin
+ */
 
-export const settings = definePluginSettings({});
+import { definePluginSettings } from "@api/Settings";
+import { OptionType } from "@utils/types";
+
+import { KeybindRecorder } from "./components/KeybindRecorder";
+
+export const keybindRecordingState = {
+    active: false
+};
+
+export const settings = definePluginSettings({
+    keybind: {
+        type: OptionType.STRING,
+        description: "Keyboard shortcut used to open Kyami Hub.",
+        default: "alt+k",
+        hidden: true
+    },
+
+    keybindRecorder: {
+        type: OptionType.COMPONENT,
+        description: "",
+        component: KeybindRecorder
+    }
+});
