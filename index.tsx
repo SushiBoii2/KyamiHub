332d2246d72c4bb90cfa3c20489125f995e9f053
sushi <glitchycement56@gmail.com>
2026-07-24 18:55:05 +1000

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,1 +1,31 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2026
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
+import definePlugin from "@utils/types";
+
+import "./styles";
+import { settings } from "./settings";
+
+export default definePlugin({
+    name: "KyamiHub",
+    description: "An all-in-one productivity hub for Discord.",
+    authors: [
+        {
+            name: "Sushi",
+            id: 0n,
+        }
+    ],
+
+    settings,
+
+    start() {
+        console.log("[Kyami] Plugin started.");
+    },
+
+    stop() {
+        console.log("[Kyami] Plugin stopped.");
+    }
+});
